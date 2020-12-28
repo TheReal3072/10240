c7a9ab2617b06453258c96062e942e0ef2b46251
Ryan2144 <70672438+Ryan2144@users.noreply.github.com>
2020-12-28 16:49:45 -0600

    40960 - Multiples of 5

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -1,5 +1,3 @@
-
-
 function HTMLActuator() {
   this.tileContainer    = document.querySelector(".tile-container");
   this.scoreContainer   = document.querySelector(".score-container");
@@ -59,7 +57,7 @@ HTMLActuator.prototype.addTile = function (tile) {
   // We can't use classlist because it somehow glitches when replacing classes
   var classes = ["tile", "tile-" + tile.value, positionClass];
 
-  if (tile.value > 2560) classes.push("tile-super");
+  if (tile.value > 41943040) classes.push("tile-super");
 
   this.applyClasses(wrapper, classes);
 
